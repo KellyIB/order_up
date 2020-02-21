4c4843db80168344a1a05a2f22d3dfe5911f6058
KellyIB <kellyibard@gmail.com>
2020-02-21 09:26:37 -0700

    add model test for ingredient and relationship test to model

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Dish, type: :model do
+RSpec.describe Ingredient, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
-    it {should validate_presence_of :description}
+    it {should validate_presence_of :calories}
   end
 
   describe "relationships" do
-    it {should belong_to :chef}
     it {should have_many :ingredient_dishes}
-    it {should have_many(:ingredients).through(:ingredient_dishes)}
+    it {should have_many(:dishes).through(:ingredient_dishes)}
   end
 end
